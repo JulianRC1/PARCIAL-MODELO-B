fd1903a6d419c71fbed2ddc414d28c979b8023fe
Julian <juldarecar2003@gmail.com>
2023-01-19 18:34:06 -0500

    Carga inicial

---
+++ src/main/java/org/main/PARCIAL1.java
@@ -0,0 +1,16 @@
+package org.main;
+
+import vista.VentanaVentas;
+
+/**
+ *
+ * @author julda
+ */
+public class PARCIAL1 {
+
+    public static void main(String[] args) 
+    {
+        VentanaVentas ventanaVentas = new VentanaVentas();
+        ventanaVentas.setVisible(true);
+    }
+}
